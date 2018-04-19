5f1184d8c3e17bd3ace426f1d4d253080ffb6c77
Cody Winchester <cjwinchester@gmail.com>
2018-04-19 17:12:35 -0600

    guess we won't use f strings after all

--- fcs.py
+++ fcs.py
@@ -1,4 +1,3 @@
-import sys
 import os
 import json
 from datetime import date
@@ -8,17 +7,15 @@ import requests
 today = date.today()
 y, m, d = today.year, str(today.month).zfill(2), str(today.day).zfill(2)
 
-url = (f'https://www.nrc.gov/reading-rm/doc-collections/'
-       'event-status/event/{y}/{y}{m}{d}en.html')
+url = ('https://www.nrc.gov/reading-rm/doc-collections/'
+       'event-status/event/{y}/{y}{m}{d}en.html'.format(y=y, m=m, d=d))
 
 text = 'fort calhoun'
 
 r = requests.get(url)
 r.raise_for_status()
 
-if text not in r.text.casefold():
-    sys.exit()
-else:
+if text in r.text.casefold():
 
     slack_hook = os.environ.get('NCC_SLACK_WEBHOOK')
 
@@ -26,7 +23,7 @@ else:
         'channel': '#fcs-alerts',
         'username': 'FCS bot',
         'icon_emoji': ':warning:',
-        'text': f'I found a new reportable event at FCS: {url}'
+        'text': 'I found a new reportable event at FCS: {}'.format(url)
     }
 
     payload_as_json = json.dumps(payload)
